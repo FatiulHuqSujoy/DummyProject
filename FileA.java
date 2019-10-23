a7a8b4547f15d0b6dcb5e2eba9b5eba05c8e218d
FatiulHuqSujoy <s.f.huq11@gmail.com>
2019-10-23 20:28:49 +0600

    FileA update5

--- FileA.java
+++ FileA.java
@@ -3,12 +3,15 @@ import what.ever;
 public class FileA{
   int x;
   public void methodAA(){
-    x=20;
+    x=21;
+    x--
     x++;
     System.out.println(x);
+    x+=10;
   }
   public void methodAB(){
     x=10;
+    x++;
     System.out.println(x);
   }
 }
